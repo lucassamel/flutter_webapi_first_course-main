f36bd5c0214a8b43866da9513611f2b028f9c9b3
Lucas Samel <Lucas.Samel@fadami.com.br>
2023-05-09 17:48:05 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'screens/home_screen/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Simple Journal',
+      theme: ThemeData(
+        primarySwatch: Colors.grey,
+        appBarTheme: const AppBarTheme(
+          elevation: 0,
+            backgroundColor: Colors.black,
+            titleTextStyle: TextStyle(color: Colors.white)),
+      ),
+      debugShowCheckedModeBanner: false,
+      darkTheme: ThemeData.dark(),
+      themeMode: ThemeMode.light,
+      initialRoute: "home",
+      routes: {
+        "home": (context) => const HomeScreen(),
+      },
+    );
+  }
+}
